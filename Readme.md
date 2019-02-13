eb2eb8c3bfcae52e446d7b915ca13c62bda7d38d
Simon DENEL <sdenel@smartwavesa.com>
2019-02-13 21:26:14 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+An image with pdflatex aimed to be used during a CI pipeline. DOn't hesitate to suggest additional packages.
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+
+apt-get update
+apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata 
+apt-get install -y texlive-latex-extra texlive-lang-french
