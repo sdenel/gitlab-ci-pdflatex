46651a49a8e58cb4ea788abb8de128fdb0048b02
Simon DENEL <sdenel@smartwavesa.com>
2019-02-19 10:29:57 +0100

    Add python3

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 apt-get update
 apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata 
-apt-get install -y texlive-latex-extra texlive-lang-french
+apt-get install -y texlive-latex-extra texlive-lang-french python3
